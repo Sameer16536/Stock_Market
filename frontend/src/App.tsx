d875928f09b3ef04740e8372cb9215f4a674f628
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-01-15 17:43:29 +0530

    Created Temporary Guide Page for Stocks

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import Home from './pages/Home'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import SignUpPage from './pages/SignUp'
 import Login from './pages/Login'
+import StockMarketGuide from './component/Guide'
 const App = () => {
   return (
     <div>
@@ -11,6 +12,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
           <Route path="/signup" element={<SignUpPage />} />
           <Route path="/login" element={<Login />} />
+          <Route path='/guide' element={<StockMarketGuide/>}/>
         </Routes>
       </BrowserRouter>
     </div>
