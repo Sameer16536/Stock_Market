ad3692466632228b56c7437cddd5ee7b58445f09
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-01-17 17:15:12 +0000

    Add cron job to run scraper every 5 minutes; update package.json with necessary dependencies

---
+++ backend/src/cronJob.ts
@@ -0,0 +1,13 @@
+import cron from 'node-cron';
+import { scrapeNSEIndia } from './scrapper';
+
+cron.schedule('*/5 * * * *', async () => {
+    console.log('Running scrapper every 5 minutes');
+    try{
+        await scrapeNSEIndia()
+        console.log('Scrapper ran successfully');
+    }
+    catch(err){
+        console.log('Error in running scrapper', err);
+    }
+});
