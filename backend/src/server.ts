7ba7760e9f63786bfcdda079b26cd913a1abb29b
Sameer Marathe <sameer.marathe16536@sakec.ac.in>
2025-04-27 08:34:31 +0000

    Refactor server.ts: remove unused import and streamline dotenv configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,10 @@
-import { dot } from 'node:test/reporters';
+
 import app from './app';
 import { configDotenv } from 'dotenv';
 import { createWebSocketServer } from './webSocket';
 import { getStockDataFromRedis } from './services/redis.service';
 import { scrapeStockHistory,saveStockData,saveStockHistory } from './services/stock.service';
-
+configDotenv();
 const PORT = process.env.PORT || 3000;
 
 const server = app.listen(PORT, () => {
